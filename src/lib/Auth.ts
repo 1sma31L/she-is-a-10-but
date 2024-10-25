bdf83fa7395f277da139366513f07d8412e7f4e5
ismail <ismailismo78@gmail.com>
2024-10-25 18:13:15 +0100

    refactor: Update Auth.ts to explicitly declare the type of 'isUserAuthenticated'

--- src/lib/Auth.ts
+++ src/lib/Auth.ts
@@ -3,7 +3,7 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { app } from "@/config/firebase";
 
 const auth = getAuth(app);
-let isUserAuthenticated;
+let isUserAuthenticated: boolean;
 onAuthStateChanged(auth, (user) => {
 	user ? (isUserAuthenticated = true) : (isUserAuthenticated = false);
 });
