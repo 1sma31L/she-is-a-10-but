2a7a4820f2b291c1ef083646df11cfbb7966a38c
ismail <ismailismo78@gmail.com>
2024-10-25 18:55:32 +0100

    refactor: Update IsAuth.tsx to use useRouter instead of next/navigation

--- src/components/IsAuth.tsx
+++ src/components/IsAuth.tsx
@@ -3,21 +3,21 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
 
-import { redirect } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 const auth = getAuth();
 
 export default function withAuth(WrappedComponent: any) {
 	return function WithAuth(props: any) {
 		const [session, setSession] = useState<boolean | null>(null); // Use local state
-
+		const router = useRouter();
 		useEffect(() => {
 			const unsubscribe = onAuthStateChanged(auth, (user) => {
 				if (user) {
 					setSession(true); // User is authenticated
 				} else {
 					setSession(false); // User is not authenticated
-					redirect("/login"); // Redirect if not authenticated
+					router.push("/login"); // Redirect if not authenticated
 				}
 			});
 
